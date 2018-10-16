a8ed32311d97bac07d548f3cf31aed1b1a6064d8
winzcom <ebun68@gmail.com>
2018-10-16 12:59:23 +0100

    get message

--- common/models/message.js
+++ common/models/message.js
@@ -1,20 +1,26 @@
-const Producer = require('nsq-strategies').Producer;
-const lookupdAddr = ['127.0.0.1:3000', '127.0.0.1:3000'];
-const opt = { strategy: Producer.ROUND_ROBIN };
+// const Producer = require('nsq-strategies').Producer;
+// const lookupdAddr = ['127.0.0.1:3000', '127.0.0.1:3000'];
+// const opt = { strategy: Producer.ROUND_ROBIN };
 
 module.exports = function(Message) {
   Message.greet = function(msg, cb) {
     process.nextTick(function() {
       msg = msg || 'hello';
-      Producer.singleton({ lookupdHTTPAddresses: lookupdAddr }, opt, (e, p) => {
-        p.produce("greeting", msg, (err) => {
+      //Producer.singleton({ lookupdHTTPAddresses: lookupdAddr }, opt, (e, p) => {
+       // p.produce("greeting", msg, (err) => {
           //TODO
           // console.log(msg,err);
-          cb(null, 'Sender says ' + msg + ' to receiver');
-        });
-      })
+            var bal  = getBalanceFromMessage(msg);
+          cb(null, 'SMS ' + msg + ' to balance' + bal);
+       // });
+      //})
     });
   };
-
-
 };
+
+var getBalanceFromMessage = function(message) {
+  var reg = message+''.match(/.bal\D*(\d+(,\d*)*)/i)[1];
+  if(reg !== null ) {
+    return ms[1].replace(',','');
+  } 
+}
